40f41c87fe3750598b11f8c4228265ff7b19f872
Sibaram samal <sibaramgithub@gmail.com>
2022-11-17 13:33:48 +0530

    Create FirstScript.py
    Adding first python script

---
+++ FirstScript.py
@@ -0,0 +1,15 @@
+#This is a comment
+#You have to add # sysmbole everytime you are commenting a line
+print("Hello Kuni! Lets Do IT.")
+
+#To execute two command in same line give ';' as separator
+print("Hello Kuni! Lets Do IT."); print("Hello Kuni! Good job.")
+
+#if you want to show your print output in different line I mean adding a newline or enter ise """
+
+print("""Hello Kuni!
+      Lets Do IT.""")
+
+
+#while you are using the shell [IDEL] dont restart it. otherwise you will loose all the pevious declared variables
+
